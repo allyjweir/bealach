65b4edf2d26b3c78d7cba87051ffef151dccf607
Ally Weir <aweir@twig-world.com>
2018-10-16 21:24:25 +0100

    Add index and list page

--- bealach/app.py
+++ bealach/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import psycopg2
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+
+@app.route('/list')
+def list_points():
+    points = []
+    with psycopg2.connect(database='bealach') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT points.name, lat, lon, regions.name \
+                     FROM points \
+                     JOIN regions ON (points.region_id=regions.id);')
+        points = cur.fetchall()
+
+    return render_template('list.html', points=points)
